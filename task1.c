dc0e3df25c5822ab5ac36ba1fe7a785c272c8a36
Ben Newman <benjamin.newman@uon.edu.au>
2019-05-02 03:25:13 +0000

    working

--- task1.c
+++ task1.c
@@ -1,6 +1,16 @@
-#include <stdiio.h>
+#include <stdio.h>
+
+void zeroInt(int *x);
 
 int main()
 {
-return 0;
+    int a=9;
+    zeroInt(&a);
+    printf("a is now %d\n", a);
+    return 0;
+}
+
+void zeroInt(int *x)
+{
+    *x=0;
 }
